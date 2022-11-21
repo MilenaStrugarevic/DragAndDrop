055e26be8ab45a940a1ea5e51040fed0b77c5532
QA36 <QA36@DESKTOP-QPQFVHR>
2022-11-21 19:39:31 +0100

    Init

---
+++ DragAndDrop/DragAndDrop/Driver/WebDriver.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium.Chrome;
+using System.Reflection;
+
+namespace DragAndDrop.Driver
+{
+    internal class WebDriver
+    {
+        public static OpenQA.Selenium.IWebDriver? Instance { get; set; }
+
+
+        public static void Initialize()
+        {
+            Instance = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            Instance.Manage().Window.Maximize();
+            Instance.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
+            Instance.Navigate().GoToUrl("https://demoqa.com/droppable");
+        }
+
+
+        public static void CleanUp()
+        {
+            Instance?.Quit();
+        }
+    }
+}
